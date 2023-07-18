bbbd3605b7eaf8cd95c8989a96b17983ea5ff148
Hildor Júnior <hildorjunior@gmail.com>
2023-07-18 14:04:12 -0300

    Update src/modules/reviews/services/FindReviewService.spec.ts
    Co-authored-by: Marcus Macedo <marcus.macedo@acad.pucrs.br>

--- src/modules/reviews/services/FindReviewService.spec.ts
+++ src/modules/reviews/services/FindReviewService.spec.ts
@@ -59,7 +59,7 @@ describe('FindReview', () => {
         street: 'test street',
         zipCode: '123123',
         complement: 'complement test',
-        descricao: 'descricao',
+        description: 'description',
       },
       cnpj: '213123',
       link: 'asdasd',
